32a75883dc13c5012f69b78bf2263e0cfaff2d96
ThanGrove <than@virginia.edu>
2017-06-15 06:57:39 -0400

    Changing URL to support page to /support

--- sites/all/themes/csc/templates/page.tpl.php
+++ sites/all/themes/csc/templates/page.tpl.php
@@ -5,7 +5,7 @@
   <ul>
     <!-- <li><a href="#"><?php print_image('explore_icon.png', 0, 0, "Explore CSC", 'class="top-icons" id="explore-icon"');?></a></li> -->
     <!-- Giving url: https://securelb.imodules.com/s/1535/16-uva/index.aspx?sid=1535&gid=16&pgid=3711&cid=6382&appealcode=KGIVECSC&bledit=1&dids=983 -->
-    <li><a target="_blank" alt="Support" class="noxhr top-anchor" href="<?php echo url('content/support'); ?>"><?php print_image('gift_icon.png', 24, 27, "Give Now", 'class="top-icons" id="calendar-icon"');?><div class="header-icon-text">Support</div></a></li>
+    <li><a target="_blank" alt="Support" class="noxhr top-anchor" href="<?php echo url('support'); ?>"><?php print_image('gift_icon.png', 24, 27, "Give Now", 'class="top-icons" id="calendar-icon"');?><div class="header-icon-text">Support</div></a></li>
     <!--<li><a id="anchor-about" alt="About Us" href="<?php echo url('content/about-us');?>" class="noxhr"><?php print_image('about_icon.png', 24, 24, "About CSC", 'class="top-icons" id="about-icon"');?><div class="header-icon-text">about</div></a></li>-->
     <li><a class="top-anchor" alt="Calendar" id="calendar-link" href="<?php echo url('csc_calendar');?>"><?php print_image('calendar_icon.png', 24, 27, "Calendar", 'class="top-icons" id="calendar-icon"');?><div class="header-icon-text">calendar</div></a></li>
     <li><a class="top-anchor" alt="Connect with us" href="<?php echo url('csc_column/contact');?>"><?php print_image('contact_icon.png', 24, 24, "Contact", 'class="top-icons" id="contact-icon"');?><div class="header-icon-text">contact</div></a></li>
