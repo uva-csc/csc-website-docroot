fbee5777e1d434f03afe693f1a117e7609e4285c
ThanGrove <than@virginia.edu>
2017-08-31 11:55:20 -0400

    Update to fix display of calendar and loading of days.

--- sites/all/modules/custom/csc_calendar/csc_calendar.js
+++ sites/all/modules/custom/csc_calendar/csc_calendar.js
@@ -2,29 +2,28 @@
     // CSC Calendar behavior to switch views using AJAX.
     Drupal.behaviors.csc_calendar = {
         attach: function(context, settings) {
+            if (context == document) {
+                // For year and month mode, redo all day links to load in the calendar slideout.
+                $(document).on('click', '#csc-popup-calendar.year a, #csc-popup-calendar.month .month.day a', function(e) {
+                    e.preventDefault();
+                    // In year view, the link to each day ends with the date string. Get that string.
+                    var pts = $(this).attr('href').split('/'); 
+                    var day = pts.pop();
+                    // Construct the calendar ajax call for that day view, get it and put in calendar, then scroll to top of div
+                    var ajax_url = '/api/csc/calendar/block/day?mini=' + day + '&ajax=1';
+                    $('#csc-right #csc-column-content').load(ajax_url, function() {
+                        Drupal.attachBehaviors('#csc-popup-calendar');
+                        $('#csc-right').get(0).scrollTop = 0;
+                    });
+               });
+            }
             $('#csc-calendar-view-form #edit-view-select').change(function(e) {
                var view_mode = $(this).val(); 
                var newurl = '/api/csc/calendar/block/' + view_mode;
                $('#csc-right #csc-column-content').load(newurl, function() {
                    Drupal.attachBehaviors('#csc-popup-calendar');
-                   // For year mode, redo all day links to load in the calendar slideout.
-                   if (view_mode == 'year') {
-                       $('#csc-popup-calendar div.year a').click(function(e) {
-                           e.preventDefault();
-                           // In year view, the link to each day ends with the date string. Get that string.
-                           var pts = $(this).attr('href').split('/'); 
-                           var day = pts.pop();
-                           // Construct the calendar ajax call for that day view, get it and put in calendar, then scroll to top of div
-                           var ajax_url = '/api/csc/calendar/block/day?mini=' + day + '&ajax=1';
-                           $('#csc-right #csc-column-content').load(ajax_url, function() {
-                                Drupal.attachBehaviors('#csc-popup-calendar');
-                                $('#csc-right').get(0).scrollTop = 0;
-                            });
-                       });
-                   }
                });
             });
         }
     };
-    
 })(jQuery);
