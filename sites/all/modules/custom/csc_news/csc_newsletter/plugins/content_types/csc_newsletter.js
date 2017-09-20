a0a07e9c4f10ee39a4b7daf9bb4fc9802a3f19a5
ThanGrove <than@virginia.edu>
2017-09-20 16:18:23 -0400

    Updates to get link to newsletter to show in sidecolumn of front page

--- sites/all/modules/custom/csc_news/csc_newsletter/plugins/content_types/csc_newsletter.js
+++ sites/all/modules/custom/csc_news/csc_newsletter/plugins/content_types/csc_newsletter.js
@@ -1,53 +1,10 @@
 (function($) {
-
     $(document).ready(function() {
-        $('.mce-button').on("click", function(e) {
-            e.preventDefault();
-
-            var f = $(this).closest("form"), email = f.find('input[name=EMAIL]');
-
-            //Check email field
-            if (email.val() == '') {
-                alert('Email field is required');
-                email.focus();
-                return;
-            }
-
-            $("#dialog-modal").dialog({
-                height : 355,
-                width : 400,
-                modal : true,
-                buttons : {
-                    "Cancel" : function() {
-                        $(this).dialog("close");
-                    },
-                    "Subscribe" : function() {
-                        if ($('#news-1').prop('checked'))
-                            $('#mce-group-16045-0').prop('checked', true);
-                        else
-                            $('#mce-group-16045-0').prop('checked', false);
-
-                        if ($('#modal-form #news-2').prop('checked'))
-                            $('#mce-group-16045-1').prop('checked', true);
-                        else
-                            $('#mce-group-16045-1').prop('checked', false);
-
-                        if ($('#modal-form #news-3').prop('checked'))
-                            $('#mce-group-16045-2').prop('checked', true);
-                        else
-                            $('#mce-group-16045-2').prop('checked', false);
-
-                        if ($('#modal-form #news-4').prop('checked'))
-                            $('#mce-group-16045-4').prop('checked', true);
-                        else
-                            $('#mce-group-16045-4').prop('checked', false);
-
-                        f.submit();
-                        $(this).dialog("close");
-                    }
-                }
-            });
-        });
+        /*
+         *  Find the #mcsignup (need to add that ID to the newsletter signup block in panoply interface)
+         *  Wrap it in a link to the e-mail sign up in another window.
+         */
+       $('div#mcsignup').wrap('<a href="http://eepurl.com/cSMDWr" target="_blank"></a>');
     });
 
 })(jQuery);
