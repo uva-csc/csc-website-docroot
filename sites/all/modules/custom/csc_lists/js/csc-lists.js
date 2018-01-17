b41d32625a5c6f2c143dcc9cdbd0f51ef9487809
ThanGrove <than@virginia.edu>
2018-01-17 14:24:58 -0500

    added code to put in header for current semester is not listed

--- sites/all/modules/custom/csc_lists/js/csc-lists.js
+++ sites/all/modules/custom/csc_lists/js/csc-lists.js
@@ -1,10 +1,15 @@
 (function($) {
-  $(function(){
-    $('#audience-filter').on('click', 'a', function(event){
-      event.preventDefault();
-      document.getElementById('audience-input').value = event.target.getAttribute('data-value');
-      document.getElementById('csc-list-form').submit();
-      return false;
+    $(function(){
+        $('#audience-filter').on('click', 'a', function(event){
+            event.preventDefault();
+            document.getElementById('audience-input').value = event.target.getAttribute('data-value');
+            document.getElementById('csc-list-form').submit();
+            return false;
+        });
+        console.log("here");
+        if (window.location.pathname == '/list/class') {
+            var ctype = $('.list-filter-wrapper a.selected').text();
+            $('p.tbap').prepend(ctype + ' ');
+        }
     });
-  });
 })(jQuery);
