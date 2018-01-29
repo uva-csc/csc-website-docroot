b24dcc90e9a476abc29d272f2c57e84093609be9
ThanGrove <than@virginia.edu>
2018-01-29 14:55:54 -0500

    adding comment

--- sites/all/modules/custom/csc_general/csc_general.js
+++ sites/all/modules/custom/csc_general/csc_general.js
@@ -3,8 +3,11 @@
     Drupal.behaviors.loginlink = {
         attach: function(context, settings) {
             if (context == document) {
-                var llink = $('.csc-login-link').detach();
-                $('.csc-panel-col-left .csc-panel-inside').append(llink);
+                // Move login link to the left bottom of left column. It is defined in page.tpl.php of the theme
+                if ($('.csc-login-link').length > 0 && $('.csc-panel-col-left .csc-panel-inside').length > 0 )  {
+                    var llink = $('.csc-login-link').detach();
+                    $('.csc-panel-col-left .csc-panel-inside').append(llink);
+                }
             }
         }
     };
