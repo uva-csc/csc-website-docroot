1a3f5fe62cf8439337681245c1c919bbe7cb18e5
ThanGrove <than@virginia.edu>
2018-03-06 12:30:25 -0500

    updates to make sure edit for shows proper repeat selection for classes

--- sites/all/modules/custom/csc_general/csc_general.js
+++ sites/all/modules/custom/csc_general/csc_general.js
@@ -30,11 +30,27 @@
     Drupal.behaviors.classnodeforms = {
         attach: function(context, settings) {
             if (context == document) {
-                // Hide the never option on any node page with date *EXCEPT* class edit pages where audience is already 'drop-in'
-                if (!$('body').is('.node-type-class') || $('.form-item-field-audience-und input[checked=checked]').val() != 'drop-in') {
+                // For class edit nodes use the stored value from csc_general.module for the repeat rule to determing which box is checked
+                // NEVER patch automatically checks that one in all cases
+                if ($('body').is('.page-node-edit.node-type-class')) {
+                    var rrule = $('input[name=rrule]').val();
+                    if (rrule.search(/UNTIL/) > -1) {
+                        $('#edit-field-date-und-0-rrule-range-of-repeat-until').attr('checked', 'checked');
+                    } else if (rrule.search(/COUNT/) > -1) {
+                         $('#edit-field-date-und-0-rrule-range-of-repeat-count').attr('checked', 'checked');
+                    } else if (rrule.search(/NEVER/) > -1) {
+                        $('#edit-field-date-und-0-rrule-range-of-repeat-never').attr('checked', 'checked');
+                    } else {
+                        $('#edit-field-date-und-0-rrule-range-of-repeat-until').attr('checked', 'checked');
+                    }
+                    if ($('.form-item-field-audience-und input[checked=checked]').val() != 'drop-in') {
+                        $('#edit-field-date-und-0-rrule-range-of-repeat-never').parent().hide();
+                    }
+                } else if($('body').is('.page-node-add-class')) {
+                    // for new class nodes just set the repeat count one as default
                     $('#edit-field-date-und-0-rrule-range-of-repeat-count').attr('checked', 'checked');
                     $('#edit-field-date-und-0-rrule-range-of-repeat-never').parent().hide();
-                } 
+                }
                 
                 // Listen to change in class type to show the never option only for classes of type drop-in
                 if ($('body').is('.page-node-add-class') || $('body').is('.page-node-edit.node-type-class')) {
